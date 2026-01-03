cf47903a4c8fe17930722e9525c8878e618700c1
bibi <kevindaniel707070@gmail.com>
2026-01-02 23:32:37 -0400

    chore(main): Borrar hello world

--- src/main/java/com/kdbf/app/App.java
+++ src/main/java/com/kdbf/app/App.java
@@ -1,10 +1,6 @@
 package com.kdbf.app;
 
-/**
- * Hello world!
- */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+  public static void main() {
+  }
 }
