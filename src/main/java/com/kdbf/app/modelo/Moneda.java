e53c5111ee4c49e73549fc7e766ff9866705c14d
bibi <kevindaniel707070@gmail.com>
2026-01-03 00:35:01 -0400

    feat(model): Crear modelo que representa una moneda

---
+++ src/main/java/com/kdbf/app/modelo/Moneda.java
@@ -0,0 +1,17 @@
+package com.kdbf.app.modelo;
+
+import java.util.Map;
+
+public class Moneda {
+
+  // Codigo de la moneda con la que se trabaje
+  public String codigoMoneda;
+  // tasas de intervambio
+  public Map<String, Double> tasas;
+
+  public Moneda(String codigoMoneda, Map<String, Double> tasas) {
+    this.codigoMoneda = codigoMoneda;
+    this.tasas = tasas;
+  }
+
+}
