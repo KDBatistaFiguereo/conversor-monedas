ec2fda7db71a17621af6ad42735a37dcecf4d522
bibi <kevindaniel707070@gmail.com>
2026-01-06 12:19:33 -0400

    refactor(servicio): Inyectar dependencias por constructor

--- src/main/java/com/kdbf/app/servicio/ConversorServicio.java
+++ src/main/java/com/kdbf/app/servicio/ConversorServicio.java
@@ -3,41 +3,35 @@ package com.kdbf.app.servicio;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.Map;
 
 import com.google.gson.Gson;
-import com.kdbf.app.config.Configuracion;
+import com.kdbf.app.config.ApiConfig;
 import com.kdbf.app.mapper.MonedaMapper;
 import com.kdbf.app.modelo.Moneda;
 import com.kdbf.app.modelo.dto.MonedaDto;
 import com.kdbf.app.servicio.fabrica.Peticiones;
+import com.kdbf.app.servicio.helper.CrearUrl;
 
 public class ConversorServicio {
-  private final String urlTotal = Configuracion.getUrlBase() + Configuracion.getLlaveApi();
-  private final HttpClient cliente = HttpClient.newHttpClient();
-  private final Gson gson = new Gson();
 
-  // /**
-  // * Metodo que muestra todas las equivalencias de una moneda dada
-  // *
-  // * @param llave Es la llave de la api
-  // * @param moneda Es el tipo de moneda (p. ej. DOP, USD)
-  // * @return Toda las conversiones posibles a todas las monedas
-  // */
-  // public Map<String, Double> mostrarCambios(String llave, String moneda) {
-  // return null;
-  // }
+  private final HttpClient cliente;
+  private final Gson gson;
+  private final Peticiones peticiones;
+  private final CrearUrl crearUrl;
+  private final ApiConfig apiConfig;
 
-  /**
-   * @param endpoint Endpoint de la api
-   * @return retorna la url con llave y endpoint apropiado
-   */
-  public String agregarEndpoint(String endpoint) {
-    return this.urlTotal + endpoint;
+  public ConversorServicio(HttpClient cliente, Gson gson, Peticiones peticiones, CrearUrl crearUrl,
+      ApiConfig apiConfig) {
+    this.cliente = cliente;
+    this.gson = gson;
+    this.peticiones = peticiones;
+    this.crearUrl = crearUrl;
+    this.apiConfig = apiConfig;
   }
 
   public Moneda pedirDatosMoneda(String codigoMoneda) {
-    HttpRequest peticion = Peticiones.crearPeticionGet(agregarEndpoint("/latest/" + codigoMoneda));
+    final String urlCompleta = crearUrl.construirUrl(this.apiConfig, "latest/" + codigoMoneda);
+    HttpRequest peticion = peticiones.crearPeticionGet(urlCompleta);
     HttpResponse<String> respuesta;
     MonedaDto monedaDto;
     Moneda moneda;
